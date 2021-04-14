dfc46236086b1bcdf6d3e92eb27fc1de5925250c
averus9 <16520480@std.stei.itb.ac.id>
2021-04-14 17:06:50 +0700

    penambahan writeCSV

--- function/csv_tools.py
+++ function/csv_tools.py
@@ -1,5 +1,4 @@
-def parseCSV(csv_file):
-    def pisah(kalimat,pemisah):
+def pisah(kalimat,pemisah):
         # kalimat = 'baris 1 \n baris 2'
         split_value = []
         tmp = ''
@@ -11,8 +10,10 @@ def parseCSV(csv_file):
                 tmp += c
         if tmp:
             split_value.append(tmp)
-
+        print(split_value)
         return split_value
+
+def parseCSV(csv_file):
     dataFile = open(csv_file)
     dataRaw = dataFile.read()
     dataFile.close()
@@ -21,18 +22,39 @@ def parseCSV(csv_file):
     headers = pisah(dataPerBaris[0],';')
     for data in dataPerBaris[1:]:
         dictData = {}
-        data = pisah(data,',')
+        data = pisah(data,';')
         i = 0
         for header in headers:
             dictData[header] = data[i]
             i += 1
         hasil.append(dictData)
 
-    print(hasil)
     return hasil
 
-def writeCSV(csv_file):
-    pass
+def writeCSV(csv_file, newData):
+    dataFile = open(csv_file,'w')
+    isiBaruFile = ""
+    #proses new_data {array of dictionary} menjadi csv
+    header = list(newData[0].keys()) #dapatkan header
 
+    def renderLine(line): #fungsi untuk merender suatu line ke dalam isiBaruFile
+        i = 0
+        global isiBaruFile
+        while i<len(line): 
+            if i == len(line) - 1:
+                isiBaruFile += line[i]
+                isiBaruFile += "\n"
+            else:
+                isiBaruFile += line[i]
+                isiBaruFile += ";"
+            i += 1
 
+    renderLine(header)#render header
+    for x in newData:#render isi
+        #x adalah sebuah dictionary
+        isi = list(x.values())
+        #render isi    
+        renderLine(isi)
+    dataFile.write(isiBaruFile)
+    dataFile.close()
     
