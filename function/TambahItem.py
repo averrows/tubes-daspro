69d1f099802fff7ba4079c7e26ff58e613b9f788
hilman1605 <hilmandzkai165@gmail.com>
2021-04-17 23:19:49 +0700

    penambahan TambahItem pada tubes

---
+++ function/TambahItem.py
@@ -0,0 +1,45 @@
+from csv_tools import parseCSV
+gadgetData = parseCSV("data" + "/gadget.csv")
+consumableData = parseCSV("data" + "/consumable.csv")
+
+def tambahitem(datas):
+    ID = input("Masukkan ID: ")
+    if ID[0] == "G":
+        gadgetData = datas["gadgetData"]
+        for tiapgadget in gadgetData:
+            ID1 = ID[1:]
+            ID2 = tiapgadget["id"][1:]
+            if ID1 == ID2 :
+                print("Gagal menambahkan item karena ID sudah ada.")
+            else :
+                gadgettambahan = {"id":"", "nama":"", "deskripsi":"", "jumlah":"", "rarity":"", "tahun ditemukan":""}
+                gadgettambahan["id"] = ID1
+                gadgettambahan["nama"] = input()
+                gadgettambahan["deskripsi"] = input()
+                gadgettambahan["jumlah"] = input()
+                gadgettambahan["rarity"] = input()
+                if gadgettambahan["rarity"] == "C" or gadgettambahan["rarity"] == "B" or gadgettambahan["rarity"] == "A" or gadgettambahan["rarity"] == "S":
+                    gadgettambahan["tahun"] = int(input())
+                    gadgetData.append(gadgettambahan)
+                else :
+                    print("Input Rarity Tidak Valid!")
+    elif ID[0] == "C":
+        consumableData = datas["consumableData"]
+        for tiapconsumable in consumableData:
+            ID1 = ID[1:]
+            ID2 = tiapconsumable["id"][1:]
+            if ID1 == ID2 :
+                print("Gagal menambahkan item karena ID sudah ada.")
+            else :
+                consumabletambahan = {"id":"", "nama":"", "deskripsi":"", "jumlah":"", "rarity":"", "tahun ditemukan":""}
+                consumabletambahan["id"] = ID1
+                consumabletambahan["nama"] = input()
+                consumabletambahan["deskripsi"] = input()
+                consumabletambahan["jumlah"] = input()
+                consumabletambahan["rarity"] = input()
+                if consumabletambahan["rarity"] == "C" or consumabletambahan["rarity"] == "B" or consumabletambahan["rarity"] == "A" or consumabletambahan["rarity"] == "S":
+                    consumableData.append(consumabletambahan)
+                else :
+                    print("Input Rarity Tidak Valid!")
+    else: #ID[0] != "C" and ID[0] != "G"
+        print("Gagal menambahkan item karena ID tidak valid.")
