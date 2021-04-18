9568b94f471d4d99a6e27145a92a1b986d8bcd7a
fayzanadia <fayzanadia@gmail.com>
2021-04-18 18:38:37 +0700

    fix fungsi cek validasi akun

--- function/login.py
+++ function/login.py
@@ -1,15 +1,16 @@
-from register import cekSama
+from CsvTools import parseCSV
+userData = parseCSV("data" + "/user.csv")
 
-def validasiAkun(username, password, userData):
-    sama_username = cekSama(username, userData, 'username')
-    sama_password = cekSama(password, userData, 'password')
-    if (sama_username == True):
-        if (sama_password == True):
-            return True
-        else:
-            return False
+def cekSama(nama, listKamus, data):
+    # untuk memeriksa apakah suatu nama sama dengan sebuah value dari data pada array of dictionary
+    count = 0
+    for i in range (len(listKamus)):
+        if (nama == listKamus[i][data]):
+            count += 1
+    if (count > 0):
+        return True
     else:
-        return False       
+        return False
 
 def login(userData):
     # input username dan password
@@ -17,12 +18,14 @@ def login(userData):
     password_login = input("Masukkan password: ")
     
     # validasi akun
-    validasiAkun(username_login, password_login, userData)
-    while (validasiAkun == False):
+    sama_username = cekSama(username_login, userData, 'username')
+    sama_password = cekSama(password_login, userData, 'password')
+    while (sama_username == False) or (sama_password == False):
         print("Masukan username atau password salah!")
         username_login = input("Masukkan username: ")
         password_login = input("masukkan password: ")
-        validasiAkun(username_login, password_login, userData)
+        sama_username = cekSama(username_login, userData, 'username')
+        sama_password = cekSama(password_login, userData, 'password')
 
     # login berhasil
     print(f"Halo {username_login}! Selamat datang di Kantong Ajaib.")
