8eeae724e589f74de4f30badce95e67a015c6eab
fayzanadia <fayzanadia@gmail.com>
2021-04-26 18:51:38 +0700

    koreksi role pada status_login

--- function/login.py
+++ function/login.py
@@ -1,5 +1,3 @@
-
-
 def cekSama(nama, listKamus, data):
     # untuk memeriksa apakah suatu nama sama dengan sebuah value dari data pada array of dictionary
     count = 0
@@ -26,10 +24,15 @@ def login(userData):
         sama_username = cekSama(username_login, userData, 'username')
         sama_password = cekSama(password_login, userData, 'password')
 
+    # cek role akun
+    for i in range (1, len(userData)):
+        if (username_login == userData[i]['username']):
+            role_login = userData[i]['role']
+
     # login berhasil
     print(f"Halo {username_login}! Selamat datang di Kantong Ajaib.")
     status_login = {
         'username': username_login,
-        'role': 'user'
+        'role': role_login
         }
     return status_login
