7bbede8daaad2efacf06717f65d13097bba31b6e
averus9 <16520480@std.stei.itb.ac.id>
2021-04-27 19:58:43 +0700

    load makin canggih

--- function/load.py
+++ function/load.py
@@ -2,6 +2,7 @@ from function.csvTools import parseCSV # pylint: disable=import-error
 import os 
 def load(folderData):
     print("Loading...")
+    fixingFolderTidakAda(folderData)
     fixingCsvTidakAda(folderData)
     required_csv =  ["user.csv","gadget.csv","consumable.csv","gadget_borrow_history.csv","gadget_return_history.csv","consumable_history.csv"]
     data = []
@@ -15,7 +16,7 @@ def createNewFile(fileName, header):
     newFile.close()
 def fixingCsvTidakAda(folderData):
     required_csv =  ["gadget.csv","consumable.csv","gadget_borrow_history.csv","gadget_return_history.csv","user.csv","consumable_history.csv"]
-    for (root, dirs, files) in os.walk(folderData,topdown=True):
+    for (root, dirs, files) in os.walk(folderData,topdown=False):
         Files = files
     headers = {
     "consumable_history.csv" :"id;id_pengambil;id_consumable;tanggal_pengambilan;jumlah",
@@ -31,3 +32,11 @@ def fixingCsvTidakAda(folderData):
             pass
         else: #jika tidak ada file tersebut
             createNewFile(folderData+"/"+x,headers[x])
+
+def fixingFolderTidakAda(folderData):
+    for (root, dirs, files) in os.walk(".",topdown=False):
+        Dirs = dirs
+    if folderData in Dirs:
+        pass
+    else: #folderData not in Dirs
+        os.mkdir("./"+folderData)
