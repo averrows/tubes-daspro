37000cccb08c9c67d8b042f7f6129d8078ed8067
averus9 <16520480@std.stei.itb.ac.id>
2021-05-02 19:33:43 +0700

    oke

--- function/ubahJumlah.py
+++ function/ubahJumlah.py
@@ -1,5 +1,10 @@
 from function.validasiTahundanJumlah import jumlahbesarkecil # pylint: disable=import-error
 from function.validasiID import IDValid, IDditemukan # pylint: disable=import-error
+from function.pinjamGadget import isIdItemAda # pylint: disable=import-error
+from function.kembalikanGadget import validasiAngka # pylint: disable=import-error
+
+
+
 
 def ubahjumlah(gadgetData,consumableData):
     # ALGORITMA
@@ -8,14 +13,44 @@ def ubahjumlah(gadgetData,consumableData):
         ID = input("Masukkan ID: ")
         if IDValid(ID):
             if ID[0] == "G":
-                if len(gadgetData) == 1:
-                # validasi data kosong    
-                    print("Maaf data tidak tersedia")
-                else:
-                    if IDditemukan(ID, gadgetData):
-                        ubahGadget(ID, gadgetData)
-                    else:
-                        print("Gagal menambahkan item karena ID tidak tersedia.")
+                ubahData(ID, gadgetData)
+            elif ID[0] == "C":
+                ubahData(ID, consumableData)
+            status = True
+        else:
+            print("Id tidak valid, masukkan ulang")
+            ID = input(">>> ")
 
-def ubahGadget(ID, gadgetData):
+def ubahData(id, data):
+    statusItem = isIdItemAda(id, data) #mengembalikan dictionary berisi status keberadaan dan indeks pada data
+    if statusItem["keberadaan"]:
+        jumlahPerubahan = getJumlah()
+        doChange(statusItem["indeks"], jumlahPerubahan, data)
+    else: #statusItem["keberadaan"] == False
+        print("Tidak ada item dengan id tersebut")
     
+def getJumlah():
+    jumlah = input("Masukkan perubahan jumlah: ")
+
+    # Cek apakah jumlah adalah suatu angka
+    while isinstance(jumlah,int):
+        print("Harap masukkan angka!")
+        jumlah = input(">>> ")
+    
+    return int(jumlah)
+
+def doChange(indeks,jumlahPerubahan, data):
+    jumlahAwal = int(data[indeks]["jumlah"])
+    jumlahAkhir = jumlahAwal + jumlahPerubahan
+    namaItem = data[indeks]["nama"]
+    if jumlahAkhir >= 0:
+        if jumlahAkhir > jumlahAwal:
+            data[indeks]["jumlah"] = str(jumlahAkhir)
+            print(f"{abs(jumlahPerubahan)} {namaItem} berhasil ditambahkan. Stok sekarang: {jumlahAkhir}")
+        elif jumlahAkhir < jumlahAwal:
+            data[indeks]["jumlah"] = str(jumlahAkhir)
+            print(f"{abs(jumlahPerubahan)} {namaItem} berhasil dibuang. Stok sekarang: {jumlahAkhir}")
+        else: #jumlahAkhir == jumlahAwal
+            print(f"Tidak ada perubahan stok {namaItem}")
+    else: #jumlahAkhir < 0
+        print(f"{abs(jumlahPerubahan)} {namaItem} gagal dibuang karena stok kurang. Stok sekarang: {jumlahAwal} (< {abs(jumlahPerubahan)})")
